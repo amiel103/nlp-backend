4505b5356d92445e7583349f2d159475ad80c194
amiel103 <84434249+amiel103@users.noreply.github.com>
2022-09-10 13:24:54 +0800

    add vercel in cors

--- fast_api.py
+++ fast_api.py
@@ -49,6 +49,7 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:3000",
+    "https://youtubecommentlabeler.vercel.app/",
 ]
 
 app.add_middleware(
